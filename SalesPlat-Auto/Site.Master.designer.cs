34a920013ccd5b89b4a64fea08735896a707ff24
200714392 <graham.gilbert007@gmail.com>
2014-11-13 18:01:52 +0200

    Say what

--- SalesPlat-Auto/Site.Master.designer.cs
+++ SalesPlat-Auto/Site.Master.designer.cs
@@ -20,14 +20,5 @@ namespace SalesPlat_Auto {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
-        
-        /// <summary>
-        /// HyperLink1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
     }
 }
